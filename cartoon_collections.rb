57b301d080820bf57e99d0ed6c396771bdbbccf6
Warren McDonald <warrenjmac@mail.com>
2019-09-30 22:44:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,10 +16,10 @@ end
   #cheese_types = ["cheddar", "gouda", "camembert"]
  
 #end
-def find_the_cheese(potentially_cheesy_items)
+def find_the_cheese(cheesy_items)
   cheeses = %w[gouda cheddar camembert]
-
-  potentially_cheesy_items.find do |maybe_cheese|
+  
+  cheesy_items.find do |maybe_cheese|
     cheeses.include?(maybe_cheese)
   end
 end
