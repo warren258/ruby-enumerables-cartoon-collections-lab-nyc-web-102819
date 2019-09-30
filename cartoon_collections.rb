079c5671307780b8420346c33e028bc4b7c1c167
Warren McDonald <warrenjmac@mail.com>
2019-09-30 19:04:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,8 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_planet# code an argument here
-
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |call| call.capitalize + '!' }
 end
 
 def long_planeteer_calls# code an argument here
