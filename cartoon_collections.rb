cb7aa6ecfff4b2d7ff5a86f325cd8add535673c0
Warren McDonald <warrenjmac@mail.com>
2019-09-30 22:04:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,14 @@ def long_planeteer_calls(planeteer_calls)
   planeteer_calls.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese
-  cheese_types = ["cheddar", "gouda", "camembert"]
+#def find_the_cheese
+  #cheese_types = ["cheddar", "gouda", "camembert"]
  
+#end
+def find_the_cheese(potentially_cheesy_items)
+  cheeses = %w[gouda cheddar camembert]
+
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
+  end
 end
