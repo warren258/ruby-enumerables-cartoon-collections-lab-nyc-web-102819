d58b8e2364d3da16649b4a7a38cdc58979e90d2c
Warren McDonald <warrenjmac@mail.com>
2019-09-30 20:09:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def long_planeteer_calls(planeteer_calls)
   planeteer_calls.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese()
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
